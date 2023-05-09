fd2cafc01b4196ece14349fa6c2de0bc8ae82914
Anai <anaiflo2003@gmail.com>
2023-05-09 12:43:42 +0200

    inicio

---
+++ src/var/Barco.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package var;
+
+/**
+ *
+ * @author 34633
+ */
+public class Barco {
+
+    
+    //constructor:
+
+    public Barco() {
+    }
+
+    
+    public void alarma();
+    
+    public void Socorro();
+    
+
+    
+}
---
+++ src/var/Var.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package var;
+
+/**
+ *
+ * @author 34633
+ */
+public class Var {
+
+    /**
+     * @param args the command line arguments
+     */
+    public void inicio(){
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Var varcoss= new Var();
+        varcoss.inicio();
+    }
+    
+}
